0d07ddb313a6eddf2fec2fd82846083661f05d11
Bechara Rizk <becharaerizk@yahoo.com>
2020-06-20 11:21:09 +0300

    edited the app

--- corona_leb_design.py
+++ corona_leb_design.py
@@ -1,9 +1,9 @@
 from kivy.app import App
-from kivy .uix.floatlayout import FloatLayout
 from kivy.properties import ObjectProperty
 from kivy.uix.popup import Popup
 from kivy.config import Config
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 
 from corona_leb import coronaLeb
 
@@ -11,25 +11,20 @@ from corona_leb import coronaLeb
 print(coronaLeb().corona_dict['recovered'])
 
 
-class myFloatLayout():
-
-    def button_pressed(self):
-        print("pressed")
-        
+class MyGrid(Widget):
+    pass
 
 
-
-class coronaApp(App):
-
-    Config.set('graphics', 'width', '300')
-    Config.set('graphics', 'height', '600')
+class MyApp(App):
 
     title = "Corona Lebanon"
 
+    Config.set('graphics', 'width', '400')
+    Config.set('graphics', 'height', '600')
+    
     def build(self):
-        return myFloatLayout()
-
+        return MyGrid()
 
 
 if __name__=="__main__":
-    coronaApp().run()
+    MyApp().run()
