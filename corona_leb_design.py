f003290ccd397537afdd5d061e683654878e8cab
Bechara Rizk <becharaerizk@yahoo.com>
2020-06-26 17:26:10 +0300

    finished base project

--- corona_leb_design.py
+++ corona_leb_design.py
@@ -20,7 +20,16 @@ class MyGrid(Widget):
     def change_text(self):
         self.ids.update_date.text = "Last Update: " + str(coronaLeb().corona_dict["update date"])
         self.ids.daily_cases.text = str(coronaLeb().corona_dict["daily cases"])
-        self.ids.tests.text = "Tests:" + str(coronaLeb().corona_dict["daily tests"])
+        self.ids.tests.text = "Tests: " + str(coronaLeb().corona_dict["daily tests"])
+        self.ids.locals_cases.text = str(coronaLeb().corona_dict["daily cases locals"])
+        self.ids.arrivals_cases.text = str(coronaLeb().corona_dict["daily cases arrivals"])
+        self.ids.total_cases_box.text = str(coronaLeb().corona_dict["total cases"])
+        self.ids.total_locals_box.text = "Locals: " + str(coronaLeb().corona_dict["total cases locals"])
+        self.ids.total_arrivals_box.text = "Arrivals: " + str(coronaLeb().corona_dict["total cases arrivals"])
+        self.ids.active_cases_box.text = str(coronaLeb().corona_dict["active cases"])
+        self.ids.deaths_box.text = str(coronaLeb().corona_dict["deaths"])
+        self.ids.reco_cases_box.text = str(coronaLeb().corona_dict["recovered"])
+        self.ids.lockdown_cases_box.text = str(coronaLeb().corona_dict["lockdown cases"])
 
 
 class MyApp(App):
